a2cc2dbc5ac7b6211ca326e2e4c1ba6f6ae577c6
Fatih Koç <mail@fatihkoc.net>
2024-07-10 11:29:46 +0300

    change push commands

--- microservice-spring-demo/docker-push.sh
+++ microservice-spring-demo/docker-push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-docker tag microservice-spring-postgres:1 ewolff/microservice-spring-postgres:latest
-docker tag microservice-spring-apache:1 ewolff/microservice-spring-apache:latest
-docker tag microservice-spring-shipping:1 ewolff/microservice-spring-shipping:latest
-docker tag microservice-spring-invoicing:1 ewolff/microservice-spring-invoicing:latest
-docker tag microservice-spring-order:1 ewolff/microservice-spring-order:latest
-docker push ewolff/microservice-spring-apache:latest
-docker push ewolff/microservice-spring-postgres:latest
-docker push ewolff/microservice-spring-shipping:latest
-docker push ewolff/microservice-spring-invoicing:latest
-docker push ewolff/microservice-spring-order:latest
+docker tag microservice-spring-postgres:1 fatihkoc/microservice-spring-postgres:latest
+docker tag microservice-spring-apache:1 fatihkoc/microservice-spring-apache:latest
+docker tag microservice-spring-shipping:1 fatihkoc/microservice-spring-shipping:latest
+docker tag microservice-spring-invoicing:1 fatihkoc/microservice-spring-invoicing:latest
+docker tag microservice-spring-order:1 fatihkoc/microservice-spring-order:latest
+docker push fatihkoc/microservice-spring-apache:latest
+docker push fatihkoc/microservice-spring-postgres:latest
+docker push fatihkoc/microservice-spring-shipping:latest
+docker push fatihkoc/microservice-spring-invoicing:latest
+docker push fatihkoc/microservice-spring-order:latest
