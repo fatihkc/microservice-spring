7f09796b76423bfd3f58c76cce0219d547fdff00
Fatih Koç <mail@fatihkoc.net>
2024-07-19 20:43:28 +0300

    url as env var

--- microservice-spring-demo/postgres/init-user-db.sh
+++ microservice-spring-demo/postgres/init-user-db.sh
@@ -3,9 +3,9 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE dbshipping;
-    GRANT ALL PRIVILEGES ON DATABASE dbshipping TO dbuser;
+    GRANT ALL PRIVILEGES ON DATABASE dbshipping TO postgres;
     CREATE DATABASE dbinvoicing;
-    GRANT ALL PRIVILEGES ON DATABASE dbinvoicing TO dbuser;
+    GRANT ALL PRIVILEGES ON DATABASE dbinvoicing TO postgres;
     CREATE DATABASE dborder;
-    GRANT ALL PRIVILEGES ON DATABASE dborder TO dbuser;
+    GRANT ALL PRIVILEGES ON DATABASE dborder TO postgres;
 EOSQL
