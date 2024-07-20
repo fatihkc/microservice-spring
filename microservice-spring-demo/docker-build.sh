a39af24f247bbdc96f5330861c631220bcb0e9e1
Fatih Koç <mail@fatihkoc.net>
2024-07-20 18:32:07 +0300

    update scripts

--- microservice-spring-demo/docker-build.sh
+++ microservice-spring-demo/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker build --platform=linux/amd64 --tag=microservice-spring-postgres:1 postgres
+#docker build --platform=linux/amd64 --tag=microservice-spring-postgres:1 postgres
 docker build --platform=linux/amd64 --tag=microservice-spring-shipping:1 microservice-spring-shipping
 docker build --platform=linux/amd64 --tag=microservice-spring-invoicing:1 microservice-spring-invoicing
 docker build --platform=linux/amd64 --tag=microservice-spring-order:1 microservice-spring-order
--- microservice-spring-demo/docker-push.sh
+++ microservice-spring-demo/docker-push.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-docker tag microservice-spring-postgres:1 fatihkoc/microservice-spring-postgres:latest
-docker tag microservice-spring-apache:1 fatihkoc/microservice-spring-apache:latest
+#docker tag microservice-spring-postgres:1 fatihkoc/microservice-spring-postgres:latest
 docker tag microservice-spring-shipping:1 fatihkoc/microservice-spring-shipping:latest
 docker tag microservice-spring-invoicing:1 fatihkoc/microservice-spring-invoicing:latest
 docker tag microservice-spring-order:1 fatihkoc/microservice-spring-order:latest
-docker push fatihkoc/microservice-spring-apache:latest
-docker push fatihkoc/microservice-spring-postgres:latest
+#docker push fatihkoc/microservice-spring-postgres:latest
 docker push fatihkoc/microservice-spring-shipping:latest
 docker push fatihkoc/microservice-spring-invoicing:latest
 docker push fatihkoc/microservice-spring-order:latest
